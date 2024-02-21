206a9d7d8f222db63bc7244c091b9f57e84a938c
AndyRoldan <andy.guevara@catolica.edu.sv>
2024-02-20 22:40:37 -0600

    Agregar archivos de proyecto.

---
+++ ApiPractica1/Controllers/equiposController.cs
@@ -0,0 +1,38 @@
+﻿using ApiPractica1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPractica1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto; 
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+        ///<sumary>
+        /// EndPoint que retorna el liustado de todos los equipos existentes
+        /// </sumary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos 
+                                           select e).ToList();
+
+            if (listadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+    }
+   
+}
---
+++ ApiPractica1/Models/equiposContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiPractica1.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<equipos> equipos { get; set;}
+    }
+}
